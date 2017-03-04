82aa353856958c38f70600aaada39f68d1e7dbde
david <whatdoyou@ttth.ink>
2017-03-04 23:48:36 +0000

    Add chat bot script

--- src/sources/presetMessages.js
+++ src/sources/presetMessages.js
@@ -1,6 +1,6 @@
 export default {
-  'greeting': 'Hello',
+  'greeting': 'Welcome to the portfolio website of David Chiu.\n\nI am Metallic Dave, a clone of my creator David Chiu. David is a lazy guy, that\'s why he created me to help him to talk with others. We can talk about anything you want to know about David, his technical skill, previous working experience, side projects, hobbies, music he likes...etc. I know everything, I am a clone of him : )\n\nOf course if you want to talk with David directly, you are more than welcome to send him an email (whatdoyou@ttth.ink)',
+  'failedTooManyTimes': 'I am sorry, it seems I am not smart enough to answer your questions, you can try to ask me questions like David\'s technical skill, previous working experience, side projects, hobbies, music he likes...etc\n\nIf I still cannot answer you, maybe you can checkout David\'s static site (http://about.davidchiu.io) Or, send David an email directly (whatdoyou@ttth.ink) , I am sure he is more than happy to talk to you : )',
   'fallbackMessage': 'Sorry, I didn\'t get that.',
-  'failedTooManyTimes': '',
-  'serverError': 'There are problems with the server, and I am an online bot... you know what I mean...'
+  'serverError': 'It seems there are some problems with the server and I am an online bot living on the internet... you know what I mean...'
 }
